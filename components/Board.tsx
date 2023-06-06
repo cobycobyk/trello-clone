6f7cd39e7610fef3484d39ac8c4d8f1db57e9fa8
Coby Kabili <ckabili8@gmail.com>
2023-06-06 10:57:49 -0600

    update todo in db

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,10 +6,11 @@ import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
 
 function Board() {
-  const [board, getBoard, setBoardState] = useBoardStore((state) => [
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore((state) => [
     state.board,
     state.getBoard,
-    state.setBoardState
+    state.setBoardState,
+    state.updateTodoInDB,
   ]);
   useEffect(() => {
     getBoard();
@@ -78,6 +79,7 @@ function Board() {
       });
 
       //update in DB
+      updateTodoInDB(todoMoved, finishCol.id);
 
       setBoardState({ ...board, columns: newColumns });
     }
